893f9dfea5d8aeb8ef43cd6bfb127135c1cb5119
virginiah894 <virgyperry@gmail.com>
2019-11-30 21:11:42 +0300

    add email stringfield

--- authform.py
+++ authform.py
@@ -1,17 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField.PasswordFieldB
-from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError,BooleanField
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
+from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError,Required
 
 class RegisterForm(FlaskForm):
   email= StringField('Your Email Address',validators=[DataRequired(),Email()])
   username = StringField('Enter your username',validators = [DataRequired(),Length(min=2,max=10)])
   
-  password = PasswordField('Password',validators = [DataRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
-  confirm_password = PasswordField('Confirm Passwords',validators = [Required()])
+  password = PasswordField('Password',validators = [DataRequired(), EqualTo('confirm_password',message = 'Passwords must match')])
+  confirm_password = PasswordField('Confirm your  Password ',validators = [Required()])
   submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
-  email = StringField('Your Email Address',validators=[Required(),Email()])
+  username = StringField('Enter your username',validators=[Required()])
   password = PasswordField('Password',validators =[Required()])
   remember = BooleanField('Remember me')
   submit = SubmitField('Login In')
+  
