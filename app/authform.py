7414df9f88c518f9ff2fc7fee190ed4f07c2ec77
virginiah894 <virgyperry@gmail.com>
2019-12-02 10:31:02 +0300

    add the form feature to add profile photos and update credentials

--- app/authform.py
+++ app/authform.py
@@ -1,7 +1,10 @@
+
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField,FileAllowed
 from wtforms import StringField,PasswordField,BooleanField,SubmitField,TextAreaField
 from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError,Required
 from app.models import User, Post
+from flask_login import current_user
 
 class RegisterForm(FlaskForm):
   email= StringField('Your Email Address',validators=[DataRequired(),Email()])
@@ -30,3 +33,23 @@ class BlogForm(FlaskForm):
   title = StringField('Title',validators=[Required()])
   content = TextAreaField('Content',validators=[Required()])
   submit = SubmitField('Submit')
+class UpdateProfileForm(FlaskForm):
+  email= StringField('Your Email Address',validators=[DataRequired(),Email()])
+  username = StringField('Enter your username',validators = [DataRequired(),Length(min=2,max=10)])
+  picture = FileField('Update your Profile Picture',validators=[FileAllowed(['jpg','png'])])
+  
+  submit = SubmitField('Update Profile')
+  # ensure no multiple accoutns with similar credentials
+  def validate_email(self,email):
+    if email.data != current_user.email:
+      user = User.query.filter_by(email=email.data).first()
+      if user:
+        raise ValidationError('An account with that email exists.Please use another one')
+  
+  def validate_username(self,username):
+
+    if username.data != current_user.username:
+
+      user = User .query.filter_by(username=username.data).first()
+      if user:
+        raise ValidationError('An account with that username exists,please choose another username')
