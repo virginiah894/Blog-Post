5c31c1443d468c4825ca353964d4ac7afab739b8
virginiah894 <virgyperry@gmail.com>
2019-12-03 00:43:26 +0300

    fix all errors

--- app/__init__.py
+++ app/__init__.py
@@ -2,26 +2,32 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_uploads import UploadSet,configure_uploads,IMAGES
-
-# flask_login import login_required,current_user
-#  ,request,redirect,url_for,abort
-
-# from ..import db,photos
-# from .forms import UpdateProfile,UpdatePitch,CommentForm
-# from . import main
-# import markdown2
+from flask_mail import Mail
+from app.config import Config
 
 
-app = Flask(__name__)
-app.config['SECRET_KEY']='924aa84d9830e3138f9caeb669c646dd'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
-db = SQLAlchemy(app)
-
-bcrypt= Bcrypt()
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = "danger"
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'userauth.login'
+login_manager.login_message_category = 'danger'
+mail = Mail()
 
 
-from app import views
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from app.userauth.views import userauth
+    from app.posts.views import posts
+    from app.main.views import main
+    app.register_blueprint(userauth)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
