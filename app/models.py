f32ddf046775acc39875583ed955caf2b559cfd1
virginiah894 <virgyperry@gmail.com>
2019-12-03 03:09:11 +0300

    add the Quote class

--- app/models.py
+++ app/models.py
@@ -25,3 +25,12 @@ class Post(db.Model):
   def __repr__(self):
     return f"Post('{self.title}','{self.date_posted}')"
 
+class Quote:
+
+  def __init__ (self,author,quote,permalink):
+
+    self.author = author
+    self.quote = quote
+    self.permalink = permalink
+    
+
