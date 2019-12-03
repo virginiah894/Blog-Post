bd5c3b28d8dc1ab3c0aad2ba2cf0140079f4c01a
virginiah894 <virgyperry@gmail.com>
2019-12-03 03:11:26 +0300

    store the emails,passwords,apis and secret keys

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
 export SECRET_KEY='924aa84d9830e3138f9caeb669c646dd'
 export SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
+export MAIL_USERNAME='veep460@gmail.com'
+export MAIL_PASSWORD='@12345@12345'
+export QUOTE_API='http://quotes.stormconsultancy.co.uk/random.json'
+python3.6 manage.py server
