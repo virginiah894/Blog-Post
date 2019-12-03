164230c061420f4876dd335fc5b542c6333ac0f8
virginiah894 <virgyperry@gmail.com>
2019-12-03 03:06:48 +0300

    add settings for the quotes api

--- app/config.py
+++ app/config.py
@@ -5,8 +5,21 @@ class Config:
     SECRET_KEY ='924aa84d9830e3138f9caeb669c646dd'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
+    MAIL_PORT = 465
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-# 
+    QUOTE_API ='http://quotes.stormconsultancy.co.uk/random.json'
+    QUOTE_API_KEY = os.environ.get('QUOTE_API_KEY')
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
